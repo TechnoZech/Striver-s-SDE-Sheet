c734015c347293bab5d684e10ca8613370fa8f2f
unknown <sharmadharmesh1998@gmail.com>
2023-06-08 23:59:30 +0530

    3rd

--- sheet.cpp
+++ sheet.cpp
@@ -85,3 +85,23 @@
 //    }
 //    return ans;
 // }
+
+
+//  Kth Largest Element in an Array  ==============>>>
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int>pq;
+
+//         for(int i=0; i<nums.size(); i++){
+//             pq.push(nums[i]);
+//         }
+
+//         for(int i=0; i<k-1 ; i++){
+//             pq.pop();
+//         }
+
+//         return pq.top();
+//     }
+// };
